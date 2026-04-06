7769a458511a9c0f20114c1ce97df5dac2fe9f37
Matias Benegas <benegas.matias21@gmail.com>
2026-04-06 13:08:57 -0300

    fix

--- src/materials/materials.module.ts
+++ src/materials/materials.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MaterialsService } from './materials.service';
 import { MaterialsController } from './materials.controller';
 import { Material } from './entities/material.entity';
+import { BusinessesModule } from '../businesses/businesses.module';
+
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Material])],
+    imports: [TypeOrmModule.forFeature([Material]), BusinessesModule],
     controllers: [MaterialsController],
     providers: [MaterialsService],
     exports: [MaterialsService],
